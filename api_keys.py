358e3ead241412fe52d15428bc2fc73bd1084566
tamyalouis <tamyalouis@gmail.com>
2025-02-15 10:46:50 -0500

    Delete api_keys.py

--- api_keys.py
+++
@@ -1,6 +0,0 @@
-# OpenWeatherMap API Key
-weather_api_key = "a1b1c021457047404e15db50db01b912"
-# Geoapify API Key
-geoapify_key = "07376d3e78a645d3a5514a68fa784f6c"
-from api_keys import geoapify_key
-print(f"Your API Key: {geoapify_key}")  # Should print your key
